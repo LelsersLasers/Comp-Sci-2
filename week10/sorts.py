f91984a85a3f9f8fa96fe741176a173d93b9571d
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-08-23 15:18:53 -0700

    Adding block comments

--- week10/sorts.py
+++ week10/sorts.py
@@ -1,16 +1,39 @@
-import random
+"""
+    Description: Collection of sorting algrothrims
+    Author: Millan
+    Date: 8/23/22
+"""
 
 
-def make_list(n: int) -> list:
+from __future__ import annotations  # type hint support
+from typing import Any  # support for explicit 'Any' type
+
+
+def make_list(n: int) -> list[int]:
+    """
+    Purpose: Makes a list of integers 0 to n
+    Parameters: n (int) the length of the list
+    Return val: A list of integers 0 to n
+    """
     return [i for i in range(n)]
 
 
-def copy_list(arr: list) -> list:
+def copy_list(arr: list[Any]) -> list[Any]:
+    """
+    Purpose: Copies a list
+    Parameters: arr (list) the list to copy
+    Return val: The copied list
+    """
     return [x for x in arr]
 
 
-def my_sort(arr: list) -> list:
-    new_arr = copy_list(arr)
+def my_sort(arr: list[Any]) -> list[Any]:
+    """
+    Purpose: Sorts a list
+    Parameters: arr (list) the list to be sorted
+    Return val: A copy of the sorted list
+    """
+    new_arr = copy_list(arr)  # does not change orginal list
     for i in range(len(new_arr) - 1):
         lowest_idx = len(new_arr) - 1
         for j in range(i, len(new_arr) - 1):
@@ -22,11 +45,18 @@ def my_sort(arr: list) -> list:
 
 
 if __name__ == "__main__":
+    import random  # for random shuffle
 
-    arr = make_list(20)
-    random.shuffle(arr)
-    
-    sorted_arr = my_sort(arr)
+    arr_len = 200
 
-    print(arr)
-    print(sorted_arr)
+    starting_arr = make_list(arr_len)
+
+    shuffled_arr = copy_list(starting_arr)
+    random.shuffle(shuffled_arr)
+
+    sorted_arr = my_sort(shuffled_arr)
+
+    print("Shuffled list:", shuffled_arr)
+    print("Sorted list:", sorted_arr)
+
+    assert sorted_arr == starting_arr
