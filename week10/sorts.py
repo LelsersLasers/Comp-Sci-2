8ef1f40b7aa03286df42aa36da825fad8e37424a
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-08-25 10:23:19 -0700

    Day 6: adding bubble, selection, insert sorts, and an attempt of quick sort

--- week10/sorts.py
+++ week10/sorts.py
@@ -29,11 +29,11 @@ def copy_list(arr: list[Any]) -> list[Any]:
 
 def my_sort(arr: list[Any]) -> list[Any]:
     """
-    Purpose: Sorts a list
+    Purpose: Sorts a list (Note: same as insertion_sort)
     Parameters: arr (list) the list to be sorted
     Return val: A copy of the sorted list
     """
-    new_arr = copy_list(arr)  # does not change orginal list
+    new_arr = copy_list(arr)
     for i in range(len(new_arr) - 1):
         lowest_idx = len(new_arr) - 1
         for j in range(i, len(new_arr) - 1):
@@ -44,19 +44,146 @@ def my_sort(arr: list[Any]) -> list[Any]:
     return new_arr
 
 
-if __name__ == "__main__":
-    import random  # for random shuffle
+def bubble_sort(arr: list[Any]) -> list[Any]:
+    """
+    This is the bubble sort algorithm:
+        - given a list arr
+        - for every item in the list, compare to the item just to the right, swap if needed
+        - keep doing the above until you go from one end of the list to the
+          other and don't make any swaps!
+    Purpose: Sorts a list (using the BubbleSort algrothrim)
+    Parameters: arr (list) the list to be sorted
+    Return val: A copy of the sorted list
+    """
+    new_arr = copy_list(arr)
+    for i in range(len(new_arr)):
+        for j in range(len(new_arr) - i - 1):
+            if new_arr[j] > new_arr[j + 1]:
+                new_arr[j], new_arr[j + 1] = new_arr[j + 1], new_arr[j]
+    return new_arr
 
-    arr_len = 200
+
+def insertion_sort(arr: list[Any]) -> list[Any]:
+    """
+    This is the insertion sort algorithm:
+        - assume item at index 0 is already "sorted"
+        - starting with item at index 1, check all items to the left and swap positions if needed
+        - do the same for item at index 2, where now items at index 0 and 1 should be in order
+        - do the same for item at index 3, where now items at index 0, 1, and 2 are in order...and so on
+
+    NOTE: Notice that, for each index, all items to the left are in order, and you are inserting the next item into the correct spot.
+    Purpose: Sorts a list (using the Insertion sort algrothrim)
+    Parameters: arr (list) the list to be sorted
+    Return val: A copy of the sorted list
+    """
+    new_arr = copy_list(arr)
+    for i in range(len(new_arr) - 1):
+        lowest_idx = len(new_arr) - 1
+        for j in range(i, len(new_arr) - 1):
+            if new_arr[j] < new_arr[lowest_idx]:
+                lowest_idx = j
+        lowest = new_arr.pop(lowest_idx)
+        new_arr.insert(i, lowest)
+    return new_arr
+
+
+def selection_sort(arr: list[Any]) -> list[Any]:
+    """
+    This is the selection sort algorithm:
+        - given a list L
+        - find the smallest number in the list, swap it to position 0
+        - find the next smallest number in the list, swap it to position 1
+        - find the next smallest number in the list, swap it to position 2
+        - And so on...
+
+    NOTE: It is called selection sort because, each time, you are selecting
+    the smallest number from the remaining unsorted elements.
+    Purpose: Sorts a list (using the Selection sort algrothrim)
+    Parameters: arr (list) the list to be sorted
+    Return val: A copy of the sorted list
+    """
+    new_arr = copy_list(arr)
+    for i in range(len(new_arr) - 1):
+        lowest_idx = len(new_arr) - 1
+        for j in range(i, len(new_arr) - 1):
+            if new_arr[j] < new_arr[lowest_idx]:
+                lowest_idx = j
+        new_arr[lowest_idx], new_arr[i] = new_arr[i], new_arr[lowest_idx]
+    return new_arr
+
+
+def quick_sort(arr: list[Any], low: int, high: int) -> None:
+    """
+    Purpose: Sorts a section of a list (part of Quick sort algrothrim)
+    Parameters: arr the list to be sorted, low the left bound of the section, high the right bound
+    Return val: None (it modifies the input list)
+    Note: hits maximum recursion depth fairly quickly
+    """
+    split = low
+    if low < high:
+        for i in range(low + 1, high + 1):
+            if arr[i] < arr[low]:
+                arr[low], arr[i] = arr[i], arr[low]
+
+        arr[low], arr[split] = arr[split], arr[low]
+        quick_sort(arr, low, split - 1)
+        quick_sort(arr, split + 1, high)
+
+
+def quick_sort_full(arr: list[Any]) -> list[Any]:
+    """
+    Purpose: Sorts a list (using the Quick sort algrothrim)
+    Parameters: arr (list) the list to be sorted
+    Return val: A copy of the sorted list
+    """
+    new_arr = copy_list(arr)
+    quick_sort(new_arr, 0, len(new_arr) - 1)
+    return new_arr
+
+
+def python_sort(arr: list[Any]) -> list[Any]:
+    """
+    Purpose: Sorts a list (using python's built-in sort())
+    Parameters: arr (list) the list to be sorted
+    Return val: A copy of the sorted list
+    """
+    return sorted(arr)
+
+
+def time_sorts(arr_len: int, sorts: list) -> list[float]:
+    """
+    Purpose: Time a sorting function/implementation
+    Parameters: The length of the list to test (int), and a list of sorting functions to use
+    Return val: A list of the times it took for all the sorts to sort the same shuffled list (-1 if a sort did not work)
+    """
+    import time  # for timing the sort
+    import random  # for shuffle
 
     starting_arr = make_list(arr_len)
 
     shuffled_arr = copy_list(starting_arr)
     random.shuffle(shuffled_arr)
 
-    sorted_arr = my_sort(shuffled_arr)
+    times: list[float] = []
 
-    print("Shuffled list:", shuffled_arr)
-    print("Sorted list:", sorted_arr)
+    for sort in sorts:
+        start_time = time.time()
+        sorted_arr = sort(shuffled_arr)
+        time_taken = time.time() - start_time
 
-    assert sorted_arr == starting_arr
+        if sorted_arr != starting_arr:
+            times.append(-1)
+        times.append(time_taken)
+
+    return times
+
+
+if __name__ == "__main__":
+
+    arr_len = 1000
+
+    sorts = [my_sort, bubble_sort, insertion_sort, selection_sort]
+    times = time_sorts(arr_len, sorts)
+
+    for i in range(len(sorts)):
+        print("%s time: %.2f" % (str(sorts[i]), times[i]))
