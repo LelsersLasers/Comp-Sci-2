de1b744a3ac38c8f7a5e09fa36d11342556a1418
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-09-06 09:17:45 -0700

    Text indicating what the sort will do

--- week10/visual-sort.py
+++ week10/visual-sort.py
@@ -12,6 +12,7 @@ TODO:
 - Text
 """
 
+
 def make_list(n: int) -> list[int]:
     """
     Purpose: Makes a list of integers 0 to n
@@ -23,28 +24,37 @@ def make_list(n: int) -> list[int]:
 
 def create_window(width: int, height: int, title: str) -> pygame.surface.Surface:
     pygame.init()
-    win = pygame.display.set_mode(
-        (width, height), pygame.SCALED
-    )
+    win = pygame.display.set_mode((width, height), pygame.SCALED)
     pygame.display.set_caption(title)
     return win
 
+
 def create_font(font_size: int, font_name: str) -> pygame.font.Font:
-    return pygame.font.Font(font_name, font_size)
+    return pygame.font.SysFont(font_name, font_size)
 
 
-def draw_graph(win: pygame.surface.Surface, arr: list[int], clock: pygame.time.Clock, fps: int, offsets: list[int], bubble_i, bubble_j) -> bool:
-    
+def draw_graph(
+    win: pygame.surface.Surface,
+    font: pygame.font.Font,
+    arr: list[int],
+    clock: pygame.time.Clock,
+    fps: int,
+    offsets: list[int],
+    bubble_i: int,
+    bubble_j: int,
+    sorted: bool,
+) -> bool:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return True
-    
+
     win.fill((0, 0, 0))
 
     for draw_i in range(len(arr)):
         bar_width = (win.get_width() - 20) / len(arr)
-        bar_height_ratio = (win.get_height() - 20) / len(arr) 
-        bar_height = int(bar_height_ratio * arr[draw_i]) + 20
+        bar_height_ratio = (win.get_height() - 100) / len(arr)
+        bar_height = int(bar_height_ratio * arr[draw_i]) + 10
 
         color = (255, 255, 255)
         if draw_i > len(arr) - bubble_i - 1:
@@ -54,9 +64,24 @@ def draw_graph(win: pygame.surface.Surface, arr: list[int], clock: pygame.time.C
         elif draw_i == bubble_j + offsets[1]:
             color = (0, 0, 255)
 
-        rect = (int(draw_i * bar_width + 10), win.get_height() - bar_height, int(bar_width), bar_height)
+        rect = (
+            int(draw_i * bar_width + 10),
+            win.get_height() - bar_height - 90,
+            int(bar_width),
+            bar_height,
+        )
         pygame.draw.rect(win, color, rect)
         pygame.draw.rect(win, (0, 0, 0), rect, 2)
+
+    text = "Sorted"
+    if not sorted:
+        if arr[bubble_j] > arr[bubble_j + 1]:
+            text = "%i > %i -- swap" % (arr[bubble_j], arr[bubble_j + 1])
+        else:
+            text = "%i <= %i -- no swap" % (arr[bubble_j], arr[bubble_j + 1])
+
+    surf_text = font.render(text, True, (255, 255, 255))
+    win.blit(surf_text, ((win.get_width() - surf_text.get_width()) / 2, 530))
 
     pygame.display.update()
     clock.tick(fps)
@@ -64,8 +89,10 @@ def draw_graph(win: pygame.surface.Surface, arr: list[int], clock: pygame.time.C
     return False
 
 
-def run(win: pygame.surface.Surface, fps: int, arr_len: int) -> None:
-    import random # for shuffle
+def run(
+    win: pygame.surface.Surface, font: pygame.font.Font, fps: int, arr_len: int
+) -> None:
+    import random  # for shuffle
 
     """
     Bubble sort
@@ -92,21 +119,23 @@ def run(win: pygame.surface.Surface, fps: int, arr_len: int) -> None:
                 arr[bubble_j], arr[bubble_j + 1] = arr[bubble_j + 1], arr[bubble_j]
                 offsets = [1, 0]
 
-            should_exit = draw_graph(win, arr, clock, fps, offsets, bubble_i, bubble_j)
+            should_exit = draw_graph(win, font, arr, clock, fps, offsets, bubble_i, bubble_j, False)
             if should_exit:
                 return
-            
+
             bubble_j += advance
         bubble_i += advance
 
-    while True:        
-        should_exit = draw_graph(win, arr, clock, fps, offsets, len(arr), -2)
+    while True:
+        should_exit = draw_graph(win, font, arr, clock, fps, offsets, len(arr), -2, True)
         if should_exit:
             return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     win = create_window(800, 600, "Visual Sort: Bubble Sort")
-    
-    run(win, 4, 8)
-    
+    font = create_font(40, "Calibri")
+
+    run(win, font, 4, 8)
+
     pygame.quit()
