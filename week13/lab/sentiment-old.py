84aba2bf10061eac8572f8649eeee541bf47cf9e
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-10-20 09:17:15 -0700

    Day 41: Week 13 - Formatting lab + updating comments and type hints

--- week13/lab/sentiment-old.py
+++ week13/lab/sentiment-old.py
@@ -10,143 +10,142 @@ from typing import Any  # support for explicit 'Any' type
 
 
 def insertion_sort(arr: list[Any], key=lambda x: x, reverse: bool = False) -> None:
-	"""
-	Purpose: Sorts a list (using the Insertion sort algrothrim)
-	Parameters: arr (list) the list to be sorted
-	Return val: None
+    """
+    Purpose: Sorts a list (using the Insertion sort algrothrim)
+    Parameters: arr (list) the list to be sorted
+    Return val: None
 
-	This is the insertion sort algorithm:
-	- assume item at index 0 is already "sorted"
-	- starting with item at index 1, check all items to the left and swap positions if needed
-	- do the same for item at index 2, where now items at index 0 and 1 should be in order
-	- do the same for item at index 3, where now items at index 0, 1, and 2 are in order...and so on
-	"""
-	for i in range(1, len(arr)):
-		val = arr[i]
-		j = i - 1
-		while j >= 0 and key(val) < key(arr[j]):
-			arr[j + 1] = arr[j]
-			j -= 1
-		arr[j + 1] = val
-	if reverse:
-		arr.reverse()
-	return
+    This is the insertion sort algorithm:
+    - assume item at index 0 is already "sorted"
+    - starting with item at index 1, check all items to the left and swap positions if needed
+    - do the same for item at index 2, where now items at index 0 and 1 should be in order
+    - do the same for item at index 3, where now items at index 0, 1, and 2 are in order...and so on
+    """
+    for i in range(1, len(arr)):
+        val = arr[i]
+        j = i - 1
+        while j >= 0 and key(val) < key(arr[j]):
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = val
+    if reverse:
+        arr.reverse()
+    return
 
 
 def binary_search(arr: list[Any], element: Any, key=lambda x: x):
-	"""
-	Purpose: Finds the desired value in a list
-	Parameters: arr (list) of anything to be searched, element (anything) that
-			is being searched for in the list, key (function) that is used to get the
-			desired value from the list (for example, if the list is a list of lists,
-			the key will get the desired value from the list to be searching for). key
-			is defaulted to just returning the value without anything special.
-	Return val: the index (int) of the found value, or the negative index of the
-			insertion point + 1
-	"""
-	low = 0
-	high = len(arr) - 1
-	while low <= high:
-		mid = (low + high) // 2
-		if key(arr[mid]) == element:
-			return mid
-		elif key(arr[mid]) > element:
-			high = mid - 1
-		else:
-			low = mid + 1
-	return -(low + 1)  # return -(insertion point + 1)
+    """
+    Purpose: Finds the desired value in a list
+    Parameters: arr (list) of anything to be searched, element (anything) that
+                    is being searched for in the list, key (function) that is used to get the
+                    desired value from the list (for example, if the list is a list of lists,
+                    the key will get the desired value from the list to be searching for). key
+                    is defaulted to just returning the value without anything special.
+    Return val: the index (int) of the found value, or the negative index of the
+                    insertion point + 1
+    """
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if key(arr[mid]) == element:
+            return mid
+        elif key(arr[mid]) > element:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -(low + 1)  # return -(insertion point + 1)
 
 
 def read_stopwords(filename: str) -> list[str]:
-	"""
-	Purpose: To read the stopwords from a file
-	Parameters: filename (str) is the name of the file to read the stopwords from
-	Return val: words (list) of the stopwords that were read from the file (sorted)
-	"""
-	stopwords = []
-	with open(filename, "r") as infile:
-		for line in infile:
-			line = line.strip().lower()
-			if line.isalpha():
-				stopwords.append(line)
-	return stopwords
+    """
+    Purpose: To read the stopwords from a file
+    Parameters: filename (str) is the name of the file to read the stopwords from
+    Return val: words (list) of the stopwords that were read from the file (sorted)
+    """
+    stopwords = []
+    with open(filename, "r") as infile:
+        for line in infile:
+            line = line.strip().lower()
+            if line.isalpha():
+                stopwords.append(line)
+    return stopwords
 
 
 def get_word_scores(filename: str, stopwords: list[str]) -> list[list[Any]]:
-	"""
+    """
     Purpose: To read reviews and score words based on the reviews
     Parameters: filename (str) is the name of the file to read the reviews from
-		and the words to ignore (list)
+                and the words to ignore (list)
     Return val: The scores for each word (unsorted)
     """
-	word_scores: list[list[Any]] = []
-	with open(filename, "r") as infile:
-		for line in infile:
-			line = line.strip().lower()
-			items = line.split()
-			try:
-				score = int(items[0]) - 2
-				review = items[1:]
-				for word in review:
-					# Is valid word
-					if word.isalpha() and binary_search(stopwords, word) < 0:
-						# Get index of word in word scores list
-						idx = binary_search(word_scores, word, lambda x: x[0])
-						if idx < 0:
-							# insert where supposed to be if not in list
-							word_scores.insert(-idx - 1, [word, score])
-						else:
-							# otherwise just increase the score
-							word_scores[idx][1] += score
-			except:
-				print("Review skipped, incorrect format")
-	return word_scores
+    word_scores: list[list[Any]] = []
+    with open(filename, "r") as infile:
+        for line in infile:
+            line = line.strip().lower()
+            items = line.split()
+            try:
+                score = int(items[0]) - 2
+                review = items[1:]
+                for word in review:
+                    # Is valid word
+                    if word.isalpha() and binary_search(stopwords, word) < 0:
+                        # Get index of word in word scores list
+                        idx = binary_search(word_scores, word, lambda x: x[0])
+                        if idx < 0:
+                            # insert where supposed to be if not in list
+                            word_scores.insert(-idx - 1, [word, score])
+                        else:
+                            # otherwise just increase the score
+                            word_scores[idx][1] += score
+            except:
+                print("Review skipped, incorrect format")
+    return word_scores
 
 
 def print_word_score(word_score: list[Any]) -> None:
-	"""
-	Purpose: Displays the scores and the word to the console
-	Parameters: word_score (list) which contains the word (str) and the score (int)
-	Return val: None
-	"""
-	print("%3i\t%s" % (word_score[1], word_score[0]))
-	return
+    """
+    Purpose: Displays the scores and the word to the console
+    Parameters: word_score (list) which contains the word (str) and the score (int)
+    Return val: None
+    """
+    print("%3i\t%s" % (word_score[1], word_score[0]))
+    return
 
 
 def display_word_scores(word_scores: list[list[Any]], number_to_display: int) -> None:
-	"""
-	Purpose: To format how the scores of the words are displayed
-	Parameters: a list of lists which contains a word (str) and a score (int)
-	Return val: None
-	"""
-	if len(word_scores) < number_to_display * 2:
-		print("All word scores:")
-		for word_score in word_scores:
-			print_word_score(word_score)
-	else:
-		print("Top 20")
-		for word_score in word_scores[:number_to_display]:
-			print_word_score(word_score)
-		print("\nBottom 20")
-		for word_score in word_scores[-number_to_display:]:
-			print_word_score(word_score)
-	return
+    """
+    Purpose: To format how the scores of the words are displayed
+    Parameters: a list of lists which contains a word (str) and a score (int)
+    Return val: None
+    """
+    if len(word_scores) < number_to_display * 2:
+        print("All word scores:")
+        for word_score in word_scores:
+            print_word_score(word_score)
+    else:
+        print("Top 20")
+        for word_score in word_scores[:number_to_display]:
+            print_word_score(word_score)
+        print("\nBottom 20")
+        for word_score in word_scores[-number_to_display:]:
+            print_word_score(word_score)
+    return
 
 
 def main():
 
-	# reads in the words to ignore (pre sorted)
-	stopwords = read_stopwords("stopwords.txt")
+    # reads in the words to ignore (pre sorted)
+    stopwords = read_stopwords("stopwords.txt")
 
-	# reads reviews and scores words based on the reviews
-	word_scores = get_word_scores("movieReviews.txt", stopwords)
+    # reads reviews and scores words based on the reviews
+    word_scores = get_word_scores("movieReviews.txt", stopwords)
 
-	# sorts the words based on their scores
-	insertion_sort(word_scores, key=lambda x: x[1], reverse=True)
+    # sorts the words based on their scores
+    insertion_sort(word_scores, key=lambda x: x[1], reverse=True)
 
-	# displays the words and their scores
-	display_word_scores(word_scores, 20)
-
+    # displays the words and their scores
+    display_word_scores(word_scores, 20)
 
 
 main()
--- week13/lab/sentiment.py
+++ week13/lab/sentiment.py
@@ -10,140 +10,140 @@ from typing import Any  # support for explicit 'Any' type
 
 
 def insertion_sort(arr: list[Any], key=lambda x: x, reverse: bool = False) -> None:
-	"""
-	Purpose: Sorts a list (using the Insertion sort algrothrim)
-	Parameters: arr (list) the list to be sorted
-	Return val: none
+    """
+    Purpose: Sorts a list (using the Insertion sort algrothrim)
+    Parameters: arr (list) that is sorted, key (optional function) a transform
+		to be applied when comparing values, reverse (optional bool) sort descending or ascending
+    Return val: None
 
-	This is the insertion sort algorithm:
-	- assume item at index 0 is already "sorted"
-	- starting with item at index 1, check all items to the left and swap positions if needed
-	- do the same for item at index 2, where now items at index 0 and 1 should be in order
-	- do the same for item at index 3, where now items at index 0, 1, and 2 are in order...and so on
-	"""
-	for i in range(1, len(arr)):
-		val = arr[i]
-		j = i - 1
-		while j >= 0 and key(val) < key(arr[j]):
-			arr[j + 1] = arr[j]
-			j -= 1
-		arr[j + 1] = val
-	if reverse:
-		arr.reverse()
-	return
+    This is the insertion sort algorithm:
+    - assume item at index 0 is already "sorted"
+    - starting with item at index 1, check all items to the left and swap positions if needed
+    - do the same for item at index 2, where now items at index 0 and 1 should be in order
+    - do the same for item at index 3, where now items at index 0, 1, and 2 are in order...and so on
+    """
+    for i in range(1, len(arr)):
+        val = arr[i]
+        j = i - 1
+        while j >= 0 and key(val) < key(arr[j]):
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = val
+    if reverse:
+        arr.reverse()
+    return
 
 
 def binary_search(arr: list[Any], element: Any, key=lambda x: x):
-	"""
-	Purpose: Finds the desired value in a list
-	Parameters: arr (list) of anything to be searched, element (anything) that
-			is being searched for in the list, key (function) that is used to get the
-			desired value from the list (for example, if the list is a list of lists,
-			the key will get the desired value from the list to be searching for). key
-			is defaulted to just returning the value without anything special.
-	Return val: the index (int) of the found value, or the negative index of the
-			insertion point + 1
-	"""
-	low = 0
-	high = len(arr) - 1
-	while low <= high:
-		mid = (low + high) // 2
-		if key(arr[mid]) == element:
-			return mid
-		elif key(arr[mid]) > element:
-			high = mid - 1
-		else:
-			low = mid + 1
-	return -(low + 1)  # return -(insertion point + 1)
+    """
+    Purpose: Finds the desired value in a list
+    Parameters: arr (list) to be searched, element (Any) that is being searched for in the
+        list, key (optional function) a transform applied to the items in the list but not
+		the element being search for
+    Return val: the index (int) of the found value, or the negative index of the insertion point + 1
+    """
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if key(arr[mid]) == element:
+            return mid
+        elif key(arr[mid]) > element:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -(low + 1)  # return -(insertion point + 1)
 
 
 def read_stopwords(filename: str) -> list[str]:
-	"""
-	Purpose: To read the stopwords from a file
-	Parameters: filename (str) is the name of the file to read the stopwords from
-	Return val: words (list) of the stopwords that were read from the file (sorted)
-	"""
-	stopwords = []
-	with open(filename, "r") as infile:
-		for line in infile:
-			line = line.strip().lower()
-			if line.isalpha():
-				stopwords.append(line)
-	return stopwords
+    """
+    Purpose: To read the stopwords from a file
+    Parameters: filename (str) is the name of the file to read the stopwords from
+    Return val: words (list) of the stopwords that were read from the file (already sorted)
+    """
+    stopwords = []
+    with open(filename, "r") as infile:
+        for line in infile:
+            line = line.strip().lower()
+            if line.isalpha():
+                stopwords.append(line)
+    return stopwords
 
 
 def get_word_scores(filename: str, stopwords: list[str]) -> dict[str, int]:
-	"""
+    """
     Purpose: To read reviews and score words based on the reviews
     Parameters: filename (str) is the name of the file to read the reviews from
-		and the words to ignore (list)
-    Return val: The scores for each word (unsorted)
+        and the words to ignore (list)
+    Return val: The scores for each word (dict with keys=words and values=scores)
     """
-	word_scores: dict[str, int] = {}
-	with open(filename, "r") as infile:
-		for line in infile:
-			line = line.strip().lower()
-			items = line.split()
-			try:
-				score = int(items[0]) - 2
-				review = items[1:]
-				for word in review:
-					# Is valid word
-					if word.isalpha() and binary_search(stopwords, word) < 0:
-						try:
-							word_scores[word] += score
-						except KeyError:
-							word_scores[word] = score
-			except:
-				print("Review skipped, incorrect format")
-	return word_scores
+    word_scores: dict[str, int] = {}
+    with open(filename, "r") as infile:
+        for line in infile:
+            line = line.strip().lower()
+            items = line.split()
+            try:
+                score = int(items[0]) - 2
+                review = items[1:]
+                for word in review:
+                    # Is valid word
+                    if word.isalpha() and binary_search(stopwords, word) < 0:
+                        try:
+                            word_scores[word] += score
+                        except KeyError:
+                            word_scores[word] = score
+            except:
+                print("Review skipped, incorrect format")
+    return word_scores
 
 
 def print_word_score(word_score: tuple[str, int]) -> None:
-	"""
-	Purpose: Displays the scores and the word to the console
-	Parameters: word_score (list) which contains the word (str) and the score (int)
-	Return val: None
-	"""
-	print("%3i\t%s" % (word_score[1], word_score[0]))
-	return
+    """
+    Purpose: Displays the scores and the word to the console
+    Parameters: word_score (tuple) which contains the word (str) and the score (int)
+    Return val: None
+    """
+    print("%3i\t%s" % (word_score[1], word_score[0]))
+    return
 
 
-def display_word_scores(word_score_tuples: list[tuple[str, int]], number_to_display: int) -> None:
-	"""
-	Purpose: To format how the scores of the words are displayed
-	Parameters: a list of lists which contains a word (str) and a score (int)
-	Return val: None
-	"""
-	if len(word_score_tuples) < number_to_display * 2:
-		print("All word scores:")
-		for word_score in word_score_tuples:
-			print_word_score(word_score)
-	else:
-		print("Top 20")
-		for word_score in word_score_tuples[:number_to_display]:
-			print_word_score(word_score)
-		print("\nBottom 20")
-		for word_score in word_score_tuples[-number_to_display:]:
-			print_word_score(word_score)
-	return
+def display_word_scores(
+    word_score_tuples: list[tuple[str, int]], number_to_display: int
+) -> None:
+    """
+    Purpose: To format how the scores of the words are displayed
+    Parameters: a list of lists which contains a word (str) and a score (int)
+    Return val: None
+    """
+    if len(word_score_tuples) < number_to_display * 2:
+        print("All word scores:")
+        for word_score in word_score_tuples:
+            print_word_score(word_score)
+    else:
+        print("Top 20")
+        for word_score in word_score_tuples[:number_to_display]:
+            print_word_score(word_score)
+        print("\nBottom 20")
+        for word_score in word_score_tuples[-number_to_display:]:
+            print_word_score(word_score)
+    return
 
 
 def main():
 
-	# reads in the words to ignore (pre sorted)
-	stopwords = read_stopwords("stopwords.txt")
+    # reads in the words to ignore (pre sorted)
+    stopwords = read_stopwords("stopwords.txt")
 
-	# reads reviews and scores words based on the reviews
-	word_scores = get_word_scores("movieReviews.txt", stopwords)
+    # reads reviews and scores words based on the reviews
+    word_scores = get_word_scores("movieReviews.txt", stopwords)
 
-	# sorts the words based on their scores
-	word_score_tuples = list(word_scores.items())
-	insertion_sort(word_score_tuples, key=lambda item: item[1], reverse=True)
+    # sorts the words based on their scores
+    word_score_tuples = list(word_scores.items()) # list[tuple[word, score]]
+	# lambda item: item[1] = sort based on score; reverse=True = sort descending
+    insertion_sort(word_score_tuples, key=lambda item: item[1], reverse=True)
 
-	# displays the words and their scores
-	display_word_scores(word_score_tuples, 20)
-
+    # displays the words and their scores
+    display_word_scores(word_score_tuples, 20)
 
 
 main()
