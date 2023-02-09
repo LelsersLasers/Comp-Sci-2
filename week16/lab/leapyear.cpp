e20b2fea541ab3172c341f2299bf54c541bd98c5
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-02-09 15:59:38 -0800

    Day 101: Week 16: doing with if/else if/else instead of 1 if

--- week16/lab/leapyear.cpp
+++ week16/lab/leapyear.cpp
@@ -11,7 +11,17 @@ int main() {
 	int year;
 	std::cin >> year;
 
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+	// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+	// 	std::cout << year << " is a leap year." << std::endl;
+	// } else {
+	// 	std::cout << year << " is not a leap year." << std::endl;
+	// }
+
+	if (year % 400 == 0) {
+		std::cout << year << " is a leap year." << std::endl;
+	} else if (year % 100 == 0) {
+		std::cout << year << " is not a leap year." << std::endl;
+	} else if (year % 4 == 0) {
 		std::cout << year << " is a leap year." << std::endl;
 	} else {
 		std::cout << year << " is not a leap year." << std::endl;
