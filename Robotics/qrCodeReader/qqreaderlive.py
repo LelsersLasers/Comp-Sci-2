c1ff951a3ba45a28178af6e933bad7cfceaf7b97
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-02-09 16:53:12 -0800

    Can sense multiple qr codes from zbar

--- Robotics/qrCodeReader/qqreaderlive.py
+++ Robotics/qrCodeReader/qqreaderlive.py
@@ -6,7 +6,6 @@
 from __future__ import annotations
 
 import subprocess
-# import sys
 import os
 
 import cv2
@@ -18,83 +17,83 @@ from PIL import Image
 defaultImagePath = "qrCode.png"
 
 
-def readQRCode(imagePath: str) -> str | None:
-	"""
-		Reads the QR code in the image at the given path.
-		Returns the contents of the QR code.
-		Returns None if the QR code could not be read.
+def readQRCode(imagePath: str) -> list[str] | None:
+    """
+    Reads the QR code in the image at the given path.
+    Returns the contents of the QR code.
+    Returns None if the QR code could not be read.
+    """
+    try:
+        output = subprocess.check_output(
+            ["zbarimg", imagePath],
+            stderr=subprocess.STDOUT  # hides output of zbarimg
+        )
 
-		Only works if there is 1 or 0 QR codes in the image.
-	"""
-	try:
-		output = subprocess.check_output(
-			["zbarimg", imagePath],
-			stderr=subprocess.STDOUT # hides output of zbarimg
-		)
+        stringOutput = output.decode("utf-8").strip()
 
-		stringOutput = output.decode("utf-8").strip()
+        outputs: list[str] = []
 
-		firstLine = stringOutput.split("\n")[0].strip()
-		parts = firstLine.split(":")
-		linkParts = parts[1:]
-		return "".join(linkParts)
+        lines = stringOutput.split("\n")
+        for line in lines:
+            line = line.strip()
+            parts = line.split(":")
+            linkParts = parts[1:]
+            fullLink = "".join(linkParts)
+            if len(fullLink) > 0:
+                outputs.append(fullLink)
 
-	except subprocess.CalledProcessError:
-		return None
+        return outputs
 
-
-
-if __name__ == '__main__':
-	# args = sys.argv[1:]
-
-	# for arg in args:
-	# 	qrCode = readQRCode(arg)
-	# 	print(f"QR Code in {arg}: {qrCode}")
+    except subprocess.CalledProcessError:
+        return None
 
 
-	cap = cv2.VideoCapture(0)
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
 
-	if not cap.isOpened():
-		print("Can't open camera.")
-		quit()
-	else:
-		print("Opened camera.")
+    if not cap.isOpened():
+        print("Can't open camera.")
+        quit()
+    else:
+        print("Opened camera.")
 
-	time.sleep(1)
+    time.sleep(1)
 
+    start = time.time()
+    delta = 1.0
 
-	start = time.time()
+    while True:
+        ret, frame = cap.read()
 
-	while True:
-		ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame.")
+            break
 
-		if not ret:
-			print("Can't receive frame.")
-			break
+        delta = time.time() - start
+        fps = 1 / delta
+        start = time.time()
 
+        print(f"\nFPS: {fps:.2f}\tDelta: {(delta * 1000):.2f} ms")
 
-		fps = 1 / (time.time() - start)
-		start = time.time()
+        cv2.imshow("Camera Feed", frame)
 
-		print(f"FPS: {fps:.2f}")
+        # Takes 60-70 ms --------------------------------------------------------------#
+        img = Image.fromarray(frame)
+        img.save(defaultImagePath)
+        # ------------------------------------------------------------------------------#
 
-		cv2.imshow("Camera Feed", frame)
+        qrCode = readQRCode(defaultImagePath)
 
-		img = Image.fromarray(frame)
-		img.save(defaultImagePath)
+        if qrCode is not None:
+            print(f"QR Code(s): {qrCode}")
+        else:
+            print("No QR code found.")
 
-		qrCode = readQRCode(defaultImagePath)
+        if cv2.waitKey(1) == ord("q"):
+            break
 
-		if qrCode is not None:
-			print(f"QR Code: {qrCode}")
-		else:
-			print("No QR code found.")
+    if os.path.exists(defaultImagePath):
+        os.remove(defaultImagePath)
 
-		if cv2.waitKey(1) == ord('q'):
-			break
-
-	if os.path.exists(defaultImagePath):
-		os.remove(defaultImagePath)
-
-	cap.release()
-	cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
--- Robotics/qrCodeReader/qrreader.py
+++ Robotics/qrCodeReader/qrreader.py
@@ -10,32 +10,32 @@ import sys
 
 
 def readQRCode(imagePath: str) -> str | None:
-	"""
-		Reads the QR code in the image at the given path.
-		Returns the contents of the QR code.
-		Returns None if the QR code could not be read.
+    """
+    Reads the QR code in the image at the given path.
+    Returns the contents of the QR code.
+    Returns None if the QR code could not be read.
 
-		Only works if there is 1 or 0 QR codes in the image.
-	"""
-	try:
-		output = subprocess.check_output(
-			["zbarimg", imagePath],
-			stderr=subprocess.STDOUT # hides output of zbarimg
-		)
+    Only works if there is 1 or 0 QR codes in the image.
+    """
+    try:
+        output = subprocess.check_output(
+            ["zbarimg", imagePath], stderr=subprocess.STDOUT  # hides output of zbarimg
+        )
 
-		stringOutput = output.decode("utf-8").strip()
+        stringOutput = output.decode("utf-8").strip()
 
-		firstLine = stringOutput.split("\n")[0].strip()
-		parts = firstLine.split(":")
-		linkParts = parts[1:]
-		return "".join(linkParts)
+        firstLine = stringOutput.split("\n")[0].strip()
+        parts = firstLine.split(":")
+        linkParts = parts[1:]
+        return "".join(linkParts)
 
-	except subprocess.CalledProcessError:
-		return None
+    except subprocess.CalledProcessError:
+        return None
 
-if __name__ == '__main__':
-	paths = sys.argv[1:]
 
-	for path in paths:
-		qrCode = readQRCode(path)
-		print(f"QR Code in {path}: {qrCode}")
+if __name__ == "__main__":
+    paths = sys.argv[1:]
+
+    for path in paths:
+        qrCode = readQRCode(path)
+        print(f"QR Code in {path}: {qrCode}")
