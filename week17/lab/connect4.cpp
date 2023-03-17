8629d4e4aafd41b33afd1f9236a88669565670da
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-03-17 10:21:50 -0700

    Adding forward declarations for every function, using namespace std

--- week17/lab/connect4.cpp
+++ week17/lab/connect4.cpp
@@ -3,35 +3,87 @@
   Author: Millan & Jerry
   Date: March 9, 2023
 */
-#include <iostream>
 
-#include <climits> // INT_MIN, INT_MAX for minimax
-#include <fstream> // file reading and writing
-#include <tuple>
-#include <utility> // pair
-#include <vector>  // python like lists for minimax
+//----------------------------------------------------------------------------//
+#include <iostream> // terminal input/output
 
+#include <climits>  // INT_MIN, INT_MAX for minimax
+#include <fstream>  // file reading and writing
+#include <tuple>    // return more than 2 things from function
+#include <utility>  // pair - return 2 things from function
+#include <vector>   // python like lists for minimax
+
+using namespace std;
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
-
-using std::pair;
-using std::tuple;
-using std::vector;
 
 const int BOARD_WIDTH = 7;
 const int BOARD_HEIGHT = 6;
 
 const int MAX_DEPTH = 8;
 
+// Use these numbers to score an unfinished board (used in minimax)
+const int COL_SCORES[7] = {1, 2, 3, 4, 3, 2, 1};
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+// Empty first so it is the default
+enum Spot { Empty, X, O };
+
+// explicit int values used when reading/writing to file
+enum ControlOptions : int { Person = 0, Easy = 1, Hard = 2 };
+
+// Game info
+struct Board {
+  Spot grid[BOARD_WIDTH][BOARD_HEIGHT];
+  bool notFilledColumn[BOARD_WIDTH];
+  Spot turn;
+};
+//----------------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------------//
+void clearScreen();
+
+char spotToChar(Spot spot, bool writingToFile = false);
+Spot charToSpot(char c);
+Spot nextTurn(Spot spot);
+
+int bestMove(Board *board);
+int minimax(Board *board, int depth, int a, int b, bool isMaximizing);
+
+void dropSpot(Board *board, int col);
+void undoMove(Board *board, int col);
+void updateFilledColumns(Board *board); 
+int getMove(Board *board, ControlOptions player);
+void move(Board *board, ControlOptions player);
+bool boardFilled(Board *board);
+bool checkWin(Board *board);
+void printGame(Board *board);
+
+ControlOptions setUpPlayer(int playerNum);
+pair<ControlOptions, ControlOptions> setUpPlayers();
+
+void saveGameData(Board *board, ControlOptions player1, ControlOptions player2);
+tuple<Board, ControlOptions, ControlOptions> readSaveGameData();
+
+vector<int> openMoves(Board *board);
+int simpleScoreBoard(Board *board);
+int bestMove(Board *board);
+int minimax(Board *board, int depth, int a, int b, bool isMaximizing);
+//----------------------------------------------------------------------------//
+
 void clearScreen() {
   // "clear" is specific to linux/mac
   system("clear");
 }
 
-// Empty first so it is the default
-enum Spot { Empty, X, O };
-
-char spotToChar(Spot spot, bool writingToFile = false) {
+char spotToChar(Spot spot, bool writingToFile) {
   switch (spot) {
   case Spot::X:
     return 'X';
@@ -71,20 +123,6 @@ Spot nextTurn(Spot spot) {
   }
 }
 
-// explicit int values used when reading/writing to file
-enum ControlOptions : int { Person = 0, Easy = 1, Hard = 2 };
-
-// Game info
-struct Board {
-  Spot grid[BOARD_WIDTH][BOARD_HEIGHT];
-  bool notFilledColumn[BOARD_WIDTH];
-  Spot turn;
-};
-
-// function declarations because they are needed in different spots
-int bestMove(Board *board);
-int minimax(Board *board, int depth, int a, int b, bool isMaximizing);
-
 void dropSpot(Board *board, int col) {
   // assumes row is not full
   for (int y = 0; y < BOARD_HEIGHT; y++) {
@@ -119,8 +157,8 @@ int getMove(Board *board, ControlOptions player) {
   int col = 0; // default value to avoid warning
   switch (player) {
   case ControlOptions::Person:
-    std::cout << "Choose column to place piece: ";
-    std::cin >> col;
+    cout << "Choose column to place piece: ";
+    cin >> col;
     col--; // index
     break;
   case ControlOptions::Easy:
@@ -139,7 +177,7 @@ int getMove(Board *board, ControlOptions player) {
     }
   }
 
-  std::cout << "Invalid move!" << std::endl;
+  cout << "Invalid move!" << endl;
   return getMove(board, player);
 }
 
@@ -249,31 +287,31 @@ void printGame(Board *board) {
   // show the game
 
   clearScreen();
-  std::cout << std::endl;
+  cout << endl;
 
   for (int y = BOARD_HEIGHT - 1; y >= 0; y--) {
-    std::cout << " | ";
+    cout << " | ";
     for (int x = 0; x < BOARD_WIDTH; x++) {
-      std::cout << spotToChar(board->grid[x][y]) << " ";
+      cout << spotToChar(board->grid[x][y]) << " ";
     }
-    std::cout << "|" << std::endl;
+    cout << "|" << endl;
   }
 
-  std::cout << " +---------------+" << std::endl;
-  std::cout << " | 1 2 3 4 5 6 7 |" << std::endl;
+  cout << " +---------------+" << endl;
+  cout << " | 1 2 3 4 5 6 7 |" << endl;
   if (board->turn != Spot::Empty) {
-    std::cout << "Current turn: " << spotToChar(board->turn) << std::endl;
+    cout << "Current turn: " << spotToChar(board->turn) << endl;
   } else {
-    std::cout << "Game over!" << std::endl;
+    cout << "Game over!" << endl;
   }
 }
 
 ControlOptions setUpPlayer(int playerNum) {
   // get control option for a player
 
-  std::cout << "Player " << playerNum << ": [P]erson, [E]asy AI, [H]ard AI? ";
+  cout << "Player " << playerNum << ": [P]erson, [E]asy AI, [H]ard AI? ";
   char playerChar;
-  std::cin >> playerChar;
+  cin >> playerChar;
 
   switch (playerChar) {
   case 'P':
@@ -287,35 +325,34 @@ ControlOptions setUpPlayer(int playerNum) {
     return ControlOptions::Hard;
   }
 
-  std::cout << "Invalid choice" << std::endl;
+  cout << "Invalid choice" << endl;
   return setUpPlayer(playerNum);
 }
 
 pair<ControlOptions, ControlOptions> setUpPlayers() {
   // get control options for both players
 
-  std::cout << "Player 1 starts (and is 'X's)" << std::endl;
-  std::cout << "Player 2 is 'O's\n" << std::endl;
+  cout << "Player 1 starts (and is 'X's)" << endl;
+  cout << "Player 2 is 'O's\n" << endl;
   ControlOptions player1 = setUpPlayer(1);
   ControlOptions player2 = setUpPlayer(2);
-  return std::make_pair(player1, player2);
+  return make_pair(player1, player2);
 }
 
-void saveGameData(Board *board, ControlOptions player1,
-                  ControlOptions player2) {
+void saveGameData(Board *board, ControlOptions player1, ControlOptions player2) {
   // write game to a file
   // always uses the same file
 
-  std::ofstream fout("gameSaves.txt", std::ios::trunc);
+  ofstream fout("gameSaves.txt", ios::trunc);
 
   for (int x = 0; x < BOARD_WIDTH; x++) {
     for (int y = 0; y < BOARD_HEIGHT; y++) {
       fout << spotToChar(board->grid[x][y], true);
     }
-    fout << std::endl;
+    fout << endl;
   }
-  fout << player1 << std::endl;
-  fout << player2 << std::endl;
+  fout << player1 << endl;
+  fout << player2 << endl;
   fout.close();
 }
 
@@ -323,13 +360,13 @@ tuple<Board, ControlOptions, ControlOptions> readSaveGameData() {
   // if there is a save file with an unfinished game, load it
   // else, create a new game
 
-  std::ifstream saveReader("gameSaves.txt");
+  ifstream saveReader("gameSaves.txt");
   Board board = {{}, {}, Spot::X};
-  std::string line;
+  string line;
 
   int x = 0;
   while (saveReader.good() && x < BOARD_WIDTH) {
-    std::getline(saveReader, line);
+    getline(saveReader, line);
     for (int y = 0; y < BOARD_HEIGHT; y++) {
       board.grid[x][y] = charToSpot(line[y]);
     }
@@ -342,11 +379,11 @@ tuple<Board, ControlOptions, ControlOptions> readSaveGameData() {
 
   ControlOptions player1, player2;
   if (saveReader.good() && x == BOARD_WIDTH && !gameOver) {
-    std::getline(saveReader, line);
-    player1 = ControlOptions(std::stoi(line));
+    getline(saveReader, line);
+    player1 = ControlOptions(stoi(line));
 
-    std::getline(saveReader, line);
-    player2 = ControlOptions(std::stoi(line));
+    getline(saveReader, line);
+    player2 = ControlOptions(stoi(line));
 
     Spot currentTurn = Spot::X;
     for (int x = 0; x < BOARD_WIDTH; x++) {
@@ -370,7 +407,7 @@ tuple<Board, ControlOptions, ControlOptions> readSaveGameData() {
 
   saveReader.close();
 
-  return std::make_tuple(board, player1, player2);
+  return make_tuple(board, player1, player2);
 }
 
 vector<int> openMoves(Board *board) {
@@ -388,15 +425,14 @@ vector<int> openMoves(Board *board) {
 int simpleScoreBoard(Board *board) {
   int score = 0;
 
-  int colScores[7] = {1, 2, 3, 4, 3, 2, 1};
 
     
   for (int x = 0; x < BOARD_WIDTH; x++) {
     for (int y = 0; y < BOARD_HEIGHT; y++) {
       if (board->grid[x][y] == Spot::X) {
-        score += colScores[x];
+        score += COL_SCORES[x];
       } else if (board->grid[x][y] == Spot::O) {
-        score -= colScores[x];
+        score -= COL_SCORES[x];
       }
     }
   }
@@ -419,7 +455,7 @@ int bestMove(Board *board) {
     dropSpot(board, col);
 
     int score = minimax(board, 0, INT_MIN, INT_MAX, !isMaximizing);
-    // std::cout << "Score[" << col << "]: " << score << std::endl;
+    // cout << "Score[" << col << "]: " << score << endl;
 
     undoMove(board, col);
 
@@ -444,10 +480,6 @@ int bestMove(Board *board) {
 
   board->turn = turn;
 
-  if (bestMoves.size() == 0) {
-    std::cout << "The AI is stumped???" << std::endl;
-    exit(1);
-  }
   int bestCol = bestMoves[rand() % bestMoves.size()];
   return bestCol;
 
@@ -525,9 +557,9 @@ int main() {
   srand(time(NULL));
 
   tuple<Board, ControlOptions, ControlOptions> saveData = readSaveGameData();
-  Board board = std::get<0>(saveData);
-  ControlOptions player1 = std::get<1>(saveData);
-  ControlOptions player2 = std::get<2>(saveData);
+  Board board = get<0>(saveData);
+  ControlOptions player1 = get<1>(saveData);
+  ControlOptions player2 = get<2>(saveData);
 
   while (true) { // break when game is over or board is filled
     printGame(&board);
@@ -540,9 +572,9 @@ int main() {
     if (won || isBoardFilled) {
       if (won) {
         char winner = spotToChar(nextTurn(board.turn));
-        std::cout << "The winner is: " << winner << std::endl;
+        cout << "The winner is: " << winner << endl;
       } else {
-        std::cout << "Tie" << std::endl;
+        cout << "Tie" << endl;
       }
       break; // skip running move()
     }
