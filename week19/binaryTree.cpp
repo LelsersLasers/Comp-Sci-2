9b054e166814ad4b7e0cbadd9c3652df7bde239e
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-05-16 13:07:48 -0700

    Binary tree: works but not balanced at all

--- week19/binaryTree.cpp
+++ week19/binaryTree.cpp
@@ -3,6 +3,10 @@
 // assert(condition) - if condition is false, the program will stop
 #include <cassert>
 
+#include <ctime>
+
+
+
 typedef int T;
 
 class Node {
@@ -10,32 +14,103 @@ class Node {
 
 public:
 	T content;
+
 	Node* left;
 	Node* right;
+
+	int heightBalance;
+
 
 	Node() {
 		content = T();
 		left = nullptr;
 		right = nullptr;
+
+		heightBalance = T();
 	}
 
 	Node(T content) {
 		this->content = content;
 		left = nullptr;
 		right = nullptr;
+
+		heightBalance = T();
 	}
 
 	void dump() {
 		// prints the content of the node and its children
-		std::cout << content << " ";
 		if (left != nullptr) {
 			left->dump();
 		}
+		
+		std::cout << content << " ";
+
 		if (right != nullptr) {
 			right->dump();
 		}
 	}
 
+	void prettyDump(int height) {
+		// prints the content of the node and its children in a tree-like format
+
+		if (right != nullptr) {
+			right->prettyDump(height + 1);
+		}
+
+		for (int i = 0; i < height; i++) {
+			std::cout << "|   ";
+		}
+
+		std::cout << content << "(" << heightBalance << ")" << std::endl;
+
+		if (left != nullptr) {
+			left->prettyDump(height + 1);
+		}
+	}
+
+	bool isLeaf() {
+		// returns true if the node is a leaf (has no children)
+		return left == nullptr && right == nullptr;
+	}
+
+	int maxHeight(int height) {
+		// returns the maximum height of the node and its children
+		// height is the height of the node
+
+		int leftHeight = 0;
+		int rightHeight = 0;
+
+		if (left != nullptr) {
+			leftHeight = left->maxHeight(height + 1);
+		}
+
+		if (right != nullptr) {
+			rightHeight = right->maxHeight(height + 1);
+		}
+
+		return std::max(height, std::max(leftHeight, rightHeight));
+	}
+
+	void updateHeightBalance(int height) {
+		// updates the height balance of the node and its children
+		// height is the height of the node
+
+		int leftHeight = 0;
+		int rightHeight = 0;
+
+		if (left != nullptr) {
+			left->updateHeightBalance(height + 1);
+			leftHeight = left->maxHeight(0);
+		}
+
+		if (right != nullptr) {
+			right->updateHeightBalance(height + 1);
+			rightHeight = right->maxHeight(0);
+		}
+
+
+		heightBalance = rightHeight - leftHeight;
+	}
 };
 
 class BinaryTree {
@@ -43,14 +118,12 @@ class BinaryTree {
 private:
 
 	Node* root;
-	long height;
 
 
 public:
 
 	BinaryTree() {
 		root = nullptr;
-		height = 0;
 	}
 
 	bool isEmpty() { return root == nullptr; }
@@ -63,43 +136,85 @@ public:
 			root = node;
 		} else {
 			Node* current = root;
-			// while (true) {
-			// 	if (content < current->content) {
-			// 		if (current->left == nullptr) {
-			// 			current->left = node;
-			// 			break;
-			// 		} else {
-			// 			current = current->left;
-			// 		}
-			// 	} else {
-			// 		if (current->right == nullptr) {
-			// 			current->right = node;
-			// 			break;
-			// 		} else {
-			// 			current = current->right;
-			// 		}
-			// 	}
-			// }
+			while (true) {
+				if (content < current->content) {
+					if (current->left == nullptr) {
+						current->left = node;
+						break;
+					} else {
+						current = current->left;
+					}
+				} else {
+					if (current->right == nullptr) {
+						current->right = node;
+						break;
+					} else {
+						current = current->right;
+					}
+				}
+			}
+			updateHeightBalances();
 		}
+	}
+
+	void updateHeightBalances() {
+		if (this->isEmpty()) {
+			return;
+		}
+
+		root->updateHeightBalance(0);
 	}
 
 	T peek() {
 		// returns the content of the root
-		// O(1) operation
-
 		assert(!this->isEmpty());
 		return root->content;
 	}
 
+	
 	void dump() {
 		// prints the content of the tree from top to bottom
-		// O(n) operation
 
 		if (this->isEmpty()) {
 			std::cout << "Empty tree" << std::endl;
 		} else {
 			std::cout << "Tree: ";
 			root->dump();
+			std::cout << std::endl;
+		}
+	}
+
+	void prettyDump() {
+		// prints the content of the tree in a tree-like format
+
+		if (this->isEmpty()) {
+			std::cout << "Empty tree" << std::endl;
+		} else {
+			std::cout << "Tree: " << std::endl;
+			root->prettyDump(0);
 		}
 	}
 };
+
+
+int randomNumber() {
+	// returns a random number between 10 and 99 (always 2 digits)
+
+	return rand() % 90 + 10;
+}
+
+int main() {
+	srand(time(nullptr));
+
+	{
+		BinaryTree tree;
+
+		for (int i = 0; i < 3; i++) {
+			tree.insert(randomNumber());
+		}
+
+		tree.dump();
+
+		tree.prettyDump();
+	}
+}
